73f698da252e90fd0e6ef315a9997ce821f4c581
Luana Neder <luananeder@gmail.com>
2022-09-02 13:02:44 -0300

    Create alo3.py

---
+++ alo3.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+x1, y1, z1 = input().split()
+x2, y2, z2 = input().split()
+
+# SOMA
+
+xs = float(x1) + float(x2)
+ys = float(y1) + float(y2)
+zs = float(z1) + float(z2)
+
+print("%.2f" % xs + " " + "%.2f" % ys + " " + "%.2f" % zs)
+
+
+
+# PROD ESC
+
+prodesc = ( float(x1) * float(x2) ) + ( float(y1) * float(y2) ) + ( float(z1) * float(z2) )
+print("%.2f" % prodesc)
+
+
+# PROD VET
+
+x1 = float(x1); y1 = float(y1); z1 = float(z1)
+x2 = float(x2); y2 = float(y2); z2 = float(z2)
+
+a = np.array([[y1, z1], [y2, z2]])
+xv = np.linalg.det(a)
+
+b = np.array([[x1, z1], [x2, z2]])
+yv = - np.linalg.det(b)
+
+c = np.array([[x1, y1], [x2, y2]])
+zv = np.linalg.det(c)
+
+print("%.2f" % xv + " " + "%.2f" % yv + " " + "%.2f" % zv)
