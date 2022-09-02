17fed4db0fb78ce09ffd9fc1fc678461a3fadf70
Luana Neder <luananeder@gmail.com>
2022-09-02 14:10:19 -0300

    l

---
+++ trabalho1/alo7.py
@@ -0,0 +1,5 @@
+n = input()
+nn = int(n + n)
+nnn = int(n + n + n)
+n = int(n)
+print(n + nn + nnn)
---
+++ trabalho1/alo8.py
@@ -0,0 +1,8 @@
+inp = input()
+
+var = inp
+for i in [2, 3, 4, 5]:
+    var = var + "---" + str(int(inp) * i)
+
+print(var)
+
