cc811ed39beefd3b449bc8a8f852a338f5c3f076
Luana <luananeder@gmail.com>
2022-09-02 15:06:27 -0300

    Add files via upload

---
+++ trabalho1/alo.py
@@ -0,0 +1,11 @@
+num = input()
+cha = []
+inv = ""
+
+for i in num:
+	cha.insert(0, i)
+
+for i in cha:
+	inv = inv + i
+
+print(inv)
---
+++ trabalho1/alo2.py
@@ -0,0 +1,7 @@
+a = input()
+
+b = a.replace("x", "cu") #; print(b)
+b = b.replace("y", "x") #; print(c)
+b = b.replace("cu", "y")
+
+print(b)
