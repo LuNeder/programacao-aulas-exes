45da559389f4bb03069562aba797c6bea3d01008
Luana Neder <luananeder@gmail.com>
2022-11-24 15:49:10 -0300

    kinda like posix time, but days instead of miliseconds

---
+++ trabalho7/2.py
@@ -0,0 +1,29 @@
+def dataine(data):
+    pass
+
+def datainb(data):
+    d = [int(x) for x in data.split("/")]
+    ltime = d[0]
+    dmonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    i = d[1]
+    while i > 0:
+        ltime = ltime + dmonth[d[1]-i]
+        i -= 1
+    return ltime
+
+
+def estacao_do_ano(data):
+    if "/" in data:
+        return datainb(data)
+    elif " " in data:
+        return dtaine(data)
+    else:
+        exit(9)
+
+
+while True:
+    data = input()
+    if data=='-1':
+        exit(0)
+    else:                
+        print(estacao_do_ano(data))
