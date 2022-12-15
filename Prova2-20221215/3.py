931710750e909beb44b4ea1c8b7118638341e479
Luana Neder <luananeder@gmail.com>
2022-12-15 16:10:53 -0300

    a

---
+++ Prova2-20221215/3.py
@@ -0,0 +1,47 @@
+i = open("message.txt", "r")
+i = [x for x in i.read().split("\n")]
+#print(i)
+dict = {}
+trad = []
+
+i[0] = "rice => arroz"
+
+for j in i:
+    if "=>" in j:
+        j = j.split(" => ")
+        dict[j[0]] = j[1]
+    else:
+        j = j.split()
+        for k in j:
+            trad.append(dict[k])
+        trad.append("\n")
+    
+
+
+while trad[len(trad) - 1] == "\n":
+    del trad[len(trad) - 1] 
+
+#print(trad)
+#print(*trad)
+
+s = ""
+
+for j in range(0, len(trad)):
+    try:
+        if trad[j] == "\n":
+            s = s + trad[j]
+        elif trad[j+1] != "\n":
+            s = s + trad[j] + " "
+        else:
+            s = s + trad[j]
+    except:
+        s = s + trad[j]
+
+
+o = open("traduction.txt", "w")
+o.write(s)
+
+# Stepik bugado do caralho
+print("# traduction.txt:")
+print(s)
+exit(0) # fecha pro stepik não rodar a leitura de arquivo dele (pq ela retorna vazia)
